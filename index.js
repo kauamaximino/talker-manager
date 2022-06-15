c809fad061f858bce8d858408e8a5d303d463a30
Kauã Maximino <kaua_maximino@outlook.com>
2022-06-15 19:10:29 -0300

    :sparkles: feat: requisito 5 em construção, criando os middlewares para as validações que o requisito pede

--- index.js
+++ index.js
@@ -24,7 +24,9 @@ const aleatoryToken = () => {
     return tokenAleatory;
 };
 
-// Middleware
+let tokenLogin = '';
+
+// Middleware para validação de email
 const validateEmail = (request, response, next) => {
   const { email } = request.body;
   const validEmail = /\S+@\S+\.\S+/.test(email);
@@ -41,6 +43,7 @@ const validateEmail = (request, response, next) => {
   return next();
 };
 
+// Middleware para validação de password
 const validatePassword = (request, response, next) => {
   const { password } = request.body;
 
@@ -51,6 +54,80 @@ const validatePassword = (request, response, next) => {
   if (password.length < 6) {
     return response.status(400)
       .send({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
+
+  return next();
+};
+
+// Middleware para validação de token
+const validateToken = (request, response, next) => {
+  const { authorization } = request.headers;
+
+  if (!authorization) {
+    return response.status(401).send({ message: 'Token não encontrado' });
+  }
+
+  if (authorization !== tokenLogin) {
+    return response.status(401).send({ message: 'Token inválido' });
+  }
+
+  return next();
+};
+
+// Middleware para validação de nome e idade
+const validateNameAndAge = (request, response, next) => {
+  const { name, age } = request.body;
+
+  if (!name) {
+    return response.status(400).send({ message: 'O campo "name" é obrigatório' });
+  }
+  if (name.length < 3) {
+    return response.status(400)
+      .send({ message: 'O "name" deve ter pelo menos 3 caracteres' });
+  }
+  if (!age) {
+    return response.status(400).send({ message: 'O campo "age" é obrigatório' });
+  }
+  if (Number(age) < 18) {
+    return response.status(400)
+      .send({ message: 'A pessoa palestrante deve ser maior de idade' });
+  }
+
+  return next();
+};
+
+// Middleware para validação do campo talk.watchedAt
+const validateTalkWatchedAt = (request, response, next) => {
+  const { talk } = request.body;
+  const validateDateRegex = /^(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d$/;
+
+  if (!talk) {
+    return response.status(400).send({ message: 'O campo "talk" é obrigatório' });
+  }
+
+  if (!talk.watchedAt) {
+    return response.status(400).send({ message: 'O campo "watchedAt" é obrigatório' });
+  }
+
+  if (!validateDateRegex.test(talk.watchedAt)) {
+    return response.status(400)
+      .send({ message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' });
+  }
+  
+    return next(); 
+};
+
+// Middleware para validação do campo talk.rate
+const validateTalkRate = (request, response, next) => {
+  const { talk: { rate } } = request.body;
+  
+  if (!rate) {
+    return response.status(400).send({ message: 'O campo "rate" é obrigatório' });
+  }
+
+  if (!Number.isInteger(rate) || rate < 0 || rate > 5) {
+    return response.status(400)
+      .send({ message: 'O campo "rate" deve ter um inteiro entre 0 e 5' });
   }
 
   return next();
@@ -74,11 +151,20 @@ app.get('/talker/:id', async (request, response) => {
 });
 
 app.post('/login', validateEmail, validatePassword, (request, response) => { 
-  const token = aleatoryToken();
+  tokenLogin = aleatoryToken();
 
-  return response.status(200).send({ token: `${token}` });
-  });
+  return response.status(200).send({ token: `${tokenLogin}` });
+});
+  
+app.post('/talker',
+  validateToken,
+  validateNameAndAge,
+  validateTalkWatchedAt,
+  validateTalkRate,
+  (request, response) => { 
+  response.status(200).send({ message: 'Pessoa palestrante cadastrada com sucesso' });
+});
 
 app.listen(PORT, () => {
-  console.log('O pai tá on');
+  console.log('o pau tá on');
 });
