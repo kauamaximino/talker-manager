5013df0f3f4296ec58b15c421e82be875b921191
Kauã Maximino <kaua_maximino@outlook.com>
2022-06-15 15:54:04 -0300

    :sparkles: feat: requisito 3 ok, criando o endpoint POST /login

--- index.js
+++ index.js
@@ -14,22 +14,19 @@ app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
-// const aleatoryToken = () => {
-//     let tokenAleatory = '';
-//     const caracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-//     for (let i = 0; i < 16; i += 1) {
-//         tokenAleatory += caracteres.charAt(Math.floor(Math.random() * caracteres.length));
-//     }
-//     return tokenAleatory;
-// };
+// gerador de Token aleatório
+const aleatoryToken = () => {
+    let tokenAleatory = '';
+    const caracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for (let i = 0; i < 16; i += 1) {
+        tokenAleatory += caracteres.charAt(Math.floor(Math.random() * caracteres.length));
+    }
+    return tokenAleatory;
+};
 
 app.get('/talker', async (_request, response) => {
-  try {
     const talker = await fs.read();
     response.status(200).send(talker);
-  } catch (error) {
-    console.log(error.message);
-  }
 });
 
 app.get('/talker/:id', async (request, response) => {
@@ -44,14 +41,12 @@ app.get('/talker/:id', async (request, response) => {
   return response.status(404).send({ message: 'Pessoa palestrante não encontrada' });
 });
 
-// app.post('/login', (request, response) => { 
-//   try {
-//     const { email, password } = request.body;
-//     response.status(200)
-//   } catch (error) {
-//     console.log(error.message);
-//   }
-// });
+app.post('/login', (request, response) => { 
+  const { email, password } = request.body;
+  const token = aleatoryToken();
+  
+  return response.status(200).send({ token: `${token}` });
+});
 
 app.listen(PORT, () => {
   console.log('O pai tá on');
